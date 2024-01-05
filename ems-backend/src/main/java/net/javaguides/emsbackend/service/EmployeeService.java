d7c67952b578fa53a233fdce486a29f601d20685
Harshit Nagila <96806109+harshit3355@users.noreply.github.com>
2024-01-05 11:55:12 +0530

    Commit new

---
+++ ems-backend/src/main/java/net/javaguides/emsbackend/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package net.javaguides.emsbackend.service;
+
+import net.javaguides.emsbackend.dto.EmployeeDto;
+import net.javaguides.emsbackend.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+
+    void deleteEmployee(Long employeeId);
+}
